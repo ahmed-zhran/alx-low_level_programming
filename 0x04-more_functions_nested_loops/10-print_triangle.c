8315ef8484ebd2b0180bcdf31ff6ab99724c5b6a
ahmed-zhran <modymoro2015@gmail.com>
2022-10-22 01:16:10 +0200

    sdfkjlld

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+*print_triangle - check a char
+*@size: integer
+*
+*Return: 0 or 2
+*/
+void print_triangle(int size)
+{
+	int a;
+	int b;
+	int c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (a = 1; a <= size; a++)
+	{
+		for (b = 1; b <= size - a; b++)
+		{
+			_putchar(' ');
+		}
+		for (c = 1; c <= a; c++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
