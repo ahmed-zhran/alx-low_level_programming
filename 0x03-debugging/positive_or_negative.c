a8001c346ef74204a56adb2ea65a678039298bc8
ahmed-zhran <modymoro2015@gmail.com>
2022-10-21 23:27:13 +0200

    fdh

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+void positive_or_negative(int i){
+    if (!n)
+    {
+        printf("%d is zero\n", n);
+    }
+    else if (n > 0)
+    {
+        printf("%d is positive\n", n);
+    }
+    else
+    {
+        printf("%d is negative\n", n);
+    }
+}
