700763d998ce7243e61e42a2ba3bf0a6496c3304
ahmed-zhran <modymoro2015@gmail.com>
2022-10-26 01:23:08 +0200

    sdlkfh

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*reset_to_98 - reset the value of a pointer to 98
+*@n: pointer to int
+*
+*Return
+*/
+void reset_to_98(int *a, int *b)
+{
+	int *tmp = b;
+	b = a;
+	a = tmp;
+}
