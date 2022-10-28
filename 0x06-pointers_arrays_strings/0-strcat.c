ed0cd63a371eaeb3ba859fa0f311e60e1ee146fd
ahmed-zhran <modymoro2015@gmail.com>
2022-10-29 01:48:42 +0200

    s;klhdfj

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,19 +6,19 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int len1 = 0, len2 = 0;
+	int len1 = 0,len2 = 0;
 	
-	while(dest[len1] != '\0')
+	while (dest[len1] != '\0')
 	{
 		len1++;
 	}
 
-	while(src[len2] != '\0')
+	while (src[len2] != '\0')
 	{
 		dest[len1++] = src[len2++];
 	}
-	
+
 	dest[len1] = '\0';
-	
-	return *dest;
+
+	return (dest);
 }
