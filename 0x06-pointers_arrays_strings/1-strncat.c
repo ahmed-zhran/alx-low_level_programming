ba4493e711a86fb2fb601e5995d6faf7258d9c95
ahmed-zhran <modymoro2015@gmail.com>
2022-10-29 02:15:02 +0200

    s;klhdfj

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*_strncat - concatnate two strings by most n bytes
+*@dest: 1st string
+*@src: 2nd string
+*@n:max bytes
+*Return: value
+*/
+char *_strcat(char *dest, char *src)
+{
+	int len1 = 0, len2 = 0;
+
+	while (dest[len1] != '\0')
+	{
+		len1++;
+	}
+
+	while (src[len2] != '\0')
+	{
+		dest[len1++] = src[len2++];
+		if (len2 > n)
+			break;
+	}
+
+	dest[len1] = '\0';
+
+	return (dest);
+}
