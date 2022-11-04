eab232d71b53282d35e6d4ca46ff6902c452c7de
ahmed-zhran <modymoro2015@gmail.com>
2022-11-04 02:24:11 +0200

    hello from alias

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,28 @@
 #include "main.h"
 /**
+*_sqrt - function
+*@n: var
+*@ans: var
+*Return: value
+*/
+int _sqrt(int n,int ans)
+{
+	if (n < 0)
+		return (-1);
+	if (!n || n == 1)
+		return (n);
+	if (!(n % ans) && ans * ans == n)
+		return (ans);
+	ans++;
+	return (_sqrt(n, ans + 1));
+}
+/**
 *_sqrt_recursion - function
 *@n: var
 *Return: value
 */
-int ans = 2;
 
 int _sqrt_recursion(int n)
 {
-	if (!n || n == 1)
-		return n;
-	else if (!(n % ans) && ans * ans == n)
-		return ans;
-	ans++;
-	return (_sqrt_recursion(n));
+	return _sqrt(n, 2);
 }
