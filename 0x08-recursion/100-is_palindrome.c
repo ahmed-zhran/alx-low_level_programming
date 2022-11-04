fa899f01b44c4f0f30ea063f018960c3fc0ac397
ahmed-zhran <modymoro2015@gmail.com>
2022-11-04 03:18:47 +0200

    hello from alias

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
  * _strlen_recursion - size
- * @s: pointer to string params
- * Return: recursion
+ * @s: var
+ * Return: int
  */
 
 int _strlen_recursion(char *s)
@@ -17,35 +16,34 @@ int _strlen_recursion(char *s)
 
 /**
  * is_p - palindrome
- * @s: pointer to string
- * @l: position
- * Return: boolena
+ * @s: var
+ * @l: var
+ * Return: int
  */
 
 int is_p(char *s, int l)
 {
 	if (l < 1)
-	{
-		return (1);
-	}
-	if (s == (s+l))
 		return (1);
 	if (*s == *(s + l))
 	{
-		return (is_p(s + 1, l - 1));
+		return (is_p(s + 1, l - 2));
 	}
 	return (0);
 }
 
 /**
  * is_palindrome - palindrome
- * @s: pointer to string
- * Return: recursion
+ * @s: var
+ * Return: int
  */
 
 int is_palindrome(char *s)
 {
 	int len = _strlen_recursion(s);
 	
-	return (is_p(s, len));
+	if(len <= 1)
+		return (1);
+	return (is_p(s, len-1));
 }
+
