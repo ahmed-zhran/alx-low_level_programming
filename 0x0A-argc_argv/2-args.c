7f22c34381d0c1aa56753636f59a8c9585771c0b
ahmed-zhran <modymoro2015@gmail.com>
2022-11-08 22:26:41 +0200

    a new alias

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+i/*
+ * File: 2-args.c
+ * Auth: Zhran
+ */
+
+#include <stdio.h>
+
+/**
+ * main - program that prints all arguments it receives.
+ * @argc: var
+ * @argv: var
+ * Return: value
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (; i<argc; i++)
+	{
+		printf("%S\n", argv[i]);
+	}
+	return (0);
+}
