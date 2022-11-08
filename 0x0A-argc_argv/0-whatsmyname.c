be50b6235535bf9ff8867dd7d382802319767aa6
ahmed-zhran <modymoro2015@gmail.com>
2022-11-08 21:32:29 +0200

    a new alias

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,11 @@
 #include <stdio.h>
 
 /**
- *main - program that prints its name, followed by a new line.
- *@argc: arg
- *@argv: arg
- *Return: Always 0.
- */
+*main - program that prints its name, followed by a new line.
+*@argc: arg
+*@argv: arg
+*Return: Always 0.
+*/
 
 int main(int agrc, char *argv[])
 {
