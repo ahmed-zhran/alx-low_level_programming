3f8a338eedb751b4b011b6312d2d0fffe0d11f2a
ahmed-zhran <modymoro2015@gmail.com>
2022-11-08 23:42:09 +0200

    a new alias

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,7 +16,7 @@
 int main(int argc, char *argv[])
 {
 	int no, sum = 0, tmp = 0;
-	
+
 	if (argc != 2)
 	{
 		printf("Error\n");
@@ -30,34 +30,34 @@ int main(int argc, char *argv[])
 	}
 	if (no >= 25)
 	{
-        	tmp = no / 25;
-	        sum += tmp;
-        	no -= (tmp * 25);
-    	}
-    	if (no >= 10)
+		tmp = no / 25;
+		sum += tmp;
+		no -= (tmp * 25);
+	}
+	if (no >= 10)
 	{
-        	tmp = no / 10;
-        	sum += tmp;
-	        no -= (tmp * 10);
+		tmp = no / 10;
+		sum += tmp;
+		no -= (tmp * 10);
 	}
 	if (no >= 5)
 	{
-	        tmp = no / 5;
-	        sum += tmp;
-	        no -= (tmp * 5);
+		tmp = no / 5;
+		sum += tmp;
+		no -= (tmp * 5);
 	}
 	if (no >= 2)
 	{
-        	tmp = no / 2;
-        	sum += tmp;
-        	no -= (tmp * 2);
+		tmp = no / 2;
+		sum += tmp;
+		no -= (tmp * 2);
     	}
-    	if (no >= 1)
+	if (no >= 1)
 	{
-        	tmp = no / 1;
-        	sum += tmp;
-        	no -= (tmp * 1);
-    	}
+		tmp = no / 1;
+		sum += tmp;
+		no -= (tmp * 1);
+	}
 	printf(sum);
 	return (0);
 }
