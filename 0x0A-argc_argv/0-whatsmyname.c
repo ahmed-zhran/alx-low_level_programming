d19a9985c8905f7dfbcade94db76213d6342e2fb
ahmed-zhran <modymoro2015@gmail.com>
2022-11-08 21:27:47 +0200

    a new alias

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+/*
+ * File: 0-whatsmyname.c
+ * Auth: Zhran
+ */
+#include <stdio.h>
+
+/**
+ * main - program that prints its name, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(int agrc, char* argv[])
+{
+	prinf("%s\n", argv[0]);
+	return (0);
+}
