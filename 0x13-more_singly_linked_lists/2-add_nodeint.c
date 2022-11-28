a66e6eb4d3445d6a7531440a2bcbbb2f152d1259
ahmed-zhran <modymoro2015@gmail.com>
2022-11-28 22:11:58 +0200

    a new alias

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,7 +9,7 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new = new malloc(sizeof(*head));
+	listint_t *new = malloc(sizeof(*head));
 
 	if (new == NULL)
 		return NULL;
