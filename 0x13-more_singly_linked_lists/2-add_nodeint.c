0d0baf3460f0d05f293c5571bf20e4261248d2e6
ahmed-zhran <modymoro2015@gmail.com>
2022-11-28 22:10:23 +0200

    a new alias

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - a new node at the beginning of a listint_t list.
+ * @head: pointer
+ * @n: var
+ * Return: value
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new = new malloc(sizeof(*head));
+
+	if (new == NULL)
+		return NULL;
+	new->n = n;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
+}
