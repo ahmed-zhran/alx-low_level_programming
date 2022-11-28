ab528dace9132f8b196501f06077e08e3931f970
ahmed-zhran <modymoro2015@gmail.com>
2022-11-28 04:13:03 +0200

    a new alias

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,18 +10,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	size_t nchar;
+	size_t n;
 
 	new = malloc(sizeof(list_t));
-	/*if (new == NULL)
-		return (NULL);*/
+	if (new == NULL)
+		return (NULL);
 
 	new->str = strdup(str);
 
-	for (nchar = 0; str[nchar]; nchar++)
-		;
+	for (n = 0; str[n]; n++)
+		
 
-	new->len = nchar;
+	new->len = n;
 	new->next = *head;
 	*head = new;
 
