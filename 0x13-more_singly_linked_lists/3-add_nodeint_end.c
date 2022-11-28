38dc05bca6ce43493b4fe261d2b1ae5a725c7a10
ahmed-zhran <modymoro2015@gmail.com>
2022-11-28 22:19:42 +0200

    a new alias

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -17,8 +17,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new->next = NULL;
 	if (*head == NULL)
 	{
-		*head == new;
-		return;
+		*head = new;
+		return (*head);
 	}
 	while (*head != NULL)
 	{
