de926b5d1534dde6b38767024fca5d2c11835d2f
ahmed-zhran <modymoro2015@gmail.com>
2022-11-28 22:18:32 +0200

    a new alias

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - a new node at the end of a listint_t list.
+ * @head: pointer
+ * @n: var
+ * Return: value
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new = malloc(sizeof(*head));
+
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head == new;
+		return;
+	}
+	while (*head != NULL)
+	{
+		*head = (*head)->next;
+	}
+	(*head) = new;
+
+	return (*head);
+}
