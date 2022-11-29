84f25acff8c045b01680911ce6cd4536fab1a067
ahmed-zhran <modymoro2015@gmail.com>
2022-11-29 08:22:41 +0200

    a new alias

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = tmp->next;
-		free(tmp);
+		free(tmp->next);
 		return (1);
 	}
 	for (; i < index - 1 && tmp != NULL; i++)
