4438d51e1aa51b5f9e6d60791c40a72a7d47d3c9
ahmed-zhran <modymoro2015@gmail.com>
2022-11-29 07:25:48 +0200

    a new alias

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	int i = 0;
-	listint_t *tmp = *head, new = malloc(sizeof(listint_t));
+	listint_t *tmp = *head, *new = malloc(sizeof(listint_t));
 	
 	new->n = n;
 	if (tmp == NULL)
