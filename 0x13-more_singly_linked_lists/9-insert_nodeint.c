e67de40890dae69c355eff59109a59b2352fc375
ahmed-zhran <modymoro2015@gmail.com>
2022-11-29 07:24:52 +0200

    a new alias

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: pointer
+ * @idx: var
+ * @n: var
+ * Return: value
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	int i = 0;
+	listint_t *tmp = *head, new = malloc(sizeof(listint_t));
+	
+	new->n = n;
+	if (tmp == NULL)
+		return (NULL);
+	for (; i < idx - 1 && tmp != NULL;)
+		tmp = tmp->next;
+	if (tmp == NULL)
+		return (NULL);
+	new->next = tmp->next;
+	tmp->next = new;
+	return (&new);
+}
