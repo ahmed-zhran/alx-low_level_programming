e2d11b11321a5d4fa9bf3a43bd0d2e1558303a49
ahmed-zhran <modymoro2015@gmail.com>
2022-11-29 08:17:57 +0200

    a new alias

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index index of a listint_t.
+ * @head: pointer
+ * @idx: var
+ * @n: var
+ * Return: value
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *tmp = *head;
+
+	if (idx == 0)
+	{
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
+	for (; i < idx - 1 && tmp != NULL; i++)
+		tmp = tmp->next;
+	if (tmp == NULL)
+		return (-1);
+	
+	tmp->next = tmp->next->next;
+	free(tmp->next);
+	return (1);
+}
