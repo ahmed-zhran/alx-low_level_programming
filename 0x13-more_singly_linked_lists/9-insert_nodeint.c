36d315fcd052034b9d47566d22b6377e6bce4f7a
ahmed-zhran <modymoro2015@gmail.com>
2022-11-29 07:40:09 +0200

    a new alias

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	for (; i < idx - 1 && tmp != NULL; i++)
 		tmp = tmp->next;
 	if (tmp == NULL)
