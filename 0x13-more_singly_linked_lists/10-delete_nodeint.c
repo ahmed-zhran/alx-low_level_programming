27849c236e55257d680156541aef102952f6e880
ahmed-zhran <modymoro2015@gmail.com>
2022-11-29 08:33:30 +0200

    a new alias

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *tmp = *head;
+	listint_t *tmp = *head, *after;
 
 	if (*head == NULL)
 		return (-1);
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		tmp = tmp->next;
 	if (tmp == NULL || tmp->next == NULL)
 		return (-1);
-	listint_t *after = (tmp->next)->next;
+	after = (tmp->next)->next;
 	free(tmp->next);
 	tmp->next = after;
 	
