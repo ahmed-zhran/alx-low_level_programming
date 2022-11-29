7531b7d61f329bb54dcdc3f4ea06e7e183d26c43
ahmed-zhran <modymoro2015@gmail.com>
2022-11-29 06:45:04 +0200

    a new alias

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t linked lis
+ * @head: var
+ * Return: value
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp = *head;
+	
+	if (temp == NULL)
+		return (0);
+	*head = (*head->next);
+	free(temp);
+	return ((*head)->n);
+}
