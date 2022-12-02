b459bd74fc729c78921603eb5715e999efce2c55
ahmed-zhran <modymoro2015@gmail.com>
2022-12-02 20:38:23 +0200

    a new alias

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: var
+ * Return: value
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int ans = 0, i = 0;
+
+	if (b == NULL)
+		return (0);
+	for (; *(b + i) != '\0'; i++)
+	{
+		if (*(b + i) == '0' || *(b + i) == '1')
+			ans += (int) (*(b + i) - 48) * pow(2, i);
+		else
+			return (0);
+	}
+	return (ans);
+}
