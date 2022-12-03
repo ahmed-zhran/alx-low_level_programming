99ae9dfe9cdf1cf1f237494a0c5a1955e3d0e4f8
ahmed-zhran <modymoro2015@gmail.com>
2022-12-03 23:40:30 +0200

    a new alias

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,12 +4,12 @@
  * set_bit - sets the value of a bit to 1 at a given index.
  * @n: var
  * @index: var
- * Return value
+ * Return: value
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 31)
-		return -1;
-	return *n |= (1 << index), 1;
+		return (-1);
+	return (*n |= (1 << index), 1);
 }
