70a6f24e6eb042532b10a6ce127136eec57389a0
ahmed-zhran <modymoro2015@gmail.com>
2022-12-03 10:57:18 +0200

    a new alias

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index.
+ * @n: var
+ * @index: var
+ * Return: value
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return index < 32 ? (n >> index) & 1 : -1;
+}
