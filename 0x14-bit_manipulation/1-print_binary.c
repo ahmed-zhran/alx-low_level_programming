9af826a71393e7a78dbebcc3bd21f859e5b2968c
ahmed-zhran <modymoro2015@gmail.com>
2022-12-03 10:48:58 +0200

    a new alias

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: var
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n <= 1)
+	{
+		_putchar((n + 48));
+		return;
+	}
+
+	print_binary(n >> 1);
+
+	_putchar(((n & 1) + 48));
+}
