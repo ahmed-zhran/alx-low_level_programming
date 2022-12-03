b0072304f77f9fef82ed02c9ffe33a25fc1cf75c
ahmed-zhran <modymoro2015@gmail.com>
2022-12-03 23:58:17 +0200

    a new alias

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,7 +11,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int ans = 0;
 
-	for (; n || m; n >>= 1, m >>=1)
+	for (; n || m; n >>= 1, m >>= 1)
 		ans += ((n & 1) != (m & 1));
 
 	return (ans);
